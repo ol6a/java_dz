613e619cee588b33e3747e9569115530fa4a5e85
ol6a <fedorova.o.a@bk.ru>
2018-09-15 20:46:25 +0300

    проверка правильности вычисления расстояния между точками

--- sandbox/src/test/java/DistanceTests.java
+++ sandbox/src/test/java/DistanceTests.java
@@ -9,9 +9,14 @@ public class DistanceTests {
         Point p2=new Point();
         p2.x=2;
         p2.y=2;
-       Assert.assertEquals((p2.rasstoyanie(p1)), 2.23606797749979);
-
+        Assert.assertEquals((p2.rasstoyanie(p1)), 2.23606797749979);
+        Point p3=new Point();
+        p3.x=1;
+        p3.y=1;
+        Point p4=new Point();
+        p4.x=3;
+        p4.y=7;
+        Assert.assertEquals((p4.rasstoyanie(p3)), 6.324555320336759);
     }
-
 
     }
