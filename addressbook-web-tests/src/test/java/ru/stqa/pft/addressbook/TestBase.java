eb7f442874279594d864bad8d81568527c927152
ol6a <fedorova.o.a@bk.ru>
2018-10-07 23:22:29 +0300

    Вспомогательные методы перенесены в TestBase
    Вспомогательные методы ContactCreationTest  и ContactDeletionTest перенесены в TestBase

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/TestBase.java
@@ -94,4 +94,54 @@ public class TestBase {
     protected void selectGroup() {
       wd.findElement(By.name("selected[]")).click();
     }
+
+    protected void returnToHomePage() {
+      wd.findElement(By.linkText("home page")).click();
+    }
+
+    protected void submitContactCreation() {
+      wd.findElement(By.xpath("(//input[@name='submit'])[2]")).click();
+    }
+
+    protected void fillContactForm(ContactData contactData) {
+      wd.findElement(By.name("firstname")).click();
+      wd.findElement(By.name("firstname")).clear();
+      wd.findElement(By.name("firstname")).sendKeys(contactData.getFirstname());
+      wd.findElement(By.name("lastname")).click();
+      wd.findElement(By.name("lastname")).clear();
+      wd.findElement(By.name("lastname")).sendKeys(contactData.getLastname());
+      wd.findElement(By.name("address")).click();
+      wd.findElement(By.name("address")).clear();
+      wd.findElement(By.name("address")).sendKeys(contactData.getAddress());
+      wd.findElement(By.name("theform")).click();
+      wd.findElement(By.name("home")).click();
+      wd.findElement(By.name("home")).clear();
+      wd.findElement(By.name("home")).sendKeys(contactData.getHometelephone());
+      wd.findElement(By.name("mobile")).click();
+      wd.findElement(By.name("mobile")).clear();
+      wd.findElement(By.name("mobile")).sendKeys(contactData.getMobiletelephone());
+      wd.findElement(By.name("work")).click();
+      wd.findElement(By.name("work")).clear();
+      wd.findElement(By.name("work")).sendKeys(contactData.getWorktelephone());
+      wd.findElement(By.name("email")).click();
+      wd.findElement(By.name("email")).clear();
+      wd.findElement(By.name("email")).sendKeys(contactData.getEmail());
+      wd.findElement(By.name("email2")).clear();
+      wd.findElement(By.name("email2")).sendKeys(contactData.getEmail2());
+      wd.findElement(By.name("email3")).clear();
+      wd.findElement(By.name("email3")).sendKeys(contactData.getEmail3());
+      wd.findElement(By.name("theform")).click();
+    }
+
+    protected void initContactCreation() {
+      wd.findElement(By.linkText("add new")).click();
+    }
+
+    protected void selectContact() {
+      wd.findElement(By.name("selected[]")).click();
+    }
+
+    protected void deleteSelectedContact() {
+      wd.findElement(By.xpath("//input[@value='Delete']")).click();
+    }
 }
