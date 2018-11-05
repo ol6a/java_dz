c7ee9c3ef4d4437b94b64f95df461a47cce36d0f
ol6a <fedorova.o.a@bk.ru>
2018-11-05 14:48:33 +0300

    Сравнение списков в тесте создания группы
    После сортировки по id достаточно сравнивать только имена, поэтому в GroupData перегенерирована функция, которая сравнивает объекты

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -1,15 +1,30 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
+
+import java.util.Comparator;
+import java.util.List;
 
 public class GroupCreationTests extends TestBase{
 
   @Test
-  public void testGroupCreation() throws Exception {
+  public void testGroupCreation() {
 
     app.getNavigationHelper().gotoGroupPage();
-    app.getGroupHelper().createGroup(new GroupData("test1", "test2", "test3"));
+    List<GroupData> before = app.getGroupHelper().getGroupList();
+    GroupData group = new GroupData("test1", "test2", "test3");
+    app.getGroupHelper().createGroup(group);
+    List<GroupData> after = app.getGroupHelper().getGroupList();
+    Assert.assertEquals(after.size(), before.size() + 1);
+
+    before.add(group);
+    Comparator<? super GroupData> byId= (o1, o2) -> Integer.compare(o1.getId(),o2.getId());
+
+    before.sort(byId);
+    after.sort(byId);
+    Assert.assertEquals(before,after);
 
 
   }
