c8b689bab890cb77f4346b3eb64fcd780d10c2f4
ol6a <fedorova.o.a@bk.ru>
2019-01-27 15:31:46 +0300

    Чтение тестовых данных для контактов из файла формата xml

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTest.java
@@ -1,25 +1,45 @@
 package ru.stqa.pft.addressbook.tests;
 
+import com.thoughtworks.xstream.XStream;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.testng.Assert.assertEquals;
 
 public class ContactCreationTest extends TestBase {
+  @DataProvider
+  public Iterator<Object[]> validContacts() throws IOException {
+    List<Object[]> list = new ArrayList<Object[]>();
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.xml")));
+    String xml="";
+    String line = reader.readLine();
+    while (line!=null) {
+      xml+=line;
+      line = reader.readLine();
+    }
+    XStream xstream= new XStream();
+    xstream.processAnnotations(ContactData.class);
+    List<ContactData> contacts= (List<ContactData>) xstream.fromXML(xml);
+    return contacts.stream().map((g)-> new Object[]{g})
+            .collect(Collectors.toList()).iterator();
+  }
 
-
-  @Test
-  public void testContactCreation() {
+  @Test (dataProvider="validContacts")
+  public void testContactCreation(ContactData contact) {
     Contacts before = app.contact().whole();
-    File photo = new File("src/test/resources/kartinka.png");
-    ContactData contact = new ContactData().withFirstname("Арина").withLastname("Розина").withAddress("г. Москва, ул. Дружбы, 2-3")
-            .withHomePhone("54-67-89").withMobilePhone("89214356789").withWorkPhone("54-45-12")
-            .withEmail("rr1@moi-uni.ru").withEmail2("rr2@moi-uni.ru").withEmail3("rr3@moi-uni.ru").withPhoto(photo);
     app.contact().create(contact);
     assertEquals(app.contact().count(),before.size()+1);
     Contacts after = app.contact().whole();
