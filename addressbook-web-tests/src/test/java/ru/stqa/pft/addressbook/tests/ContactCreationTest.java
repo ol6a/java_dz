1b6d426f35252524d3dc345540d9457a95c049bb
ol6a <fedorova.o.a@bk.ru>
2019-01-27 16:05:26 +0300

    Автоматическое закрытие файлов после использования

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTest.java
@@ -23,18 +23,19 @@ public class ContactCreationTest extends TestBase {
   @DataProvider
   public Iterator<Object[]> validContacts() throws IOException {
     List<Object[]> list = new ArrayList<Object[]>();
-    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.xml")));
-    String xml="";
-    String line = reader.readLine();
-    while (line!=null) {
-      xml+=line;
-      line = reader.readLine();
+    try(BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.xml")))) {
+      String xml = "";
+      String line = reader.readLine();
+      while (line != null) {
+        xml += line;
+        line = reader.readLine();
+      }
+      XStream xstream = new XStream();
+      xstream.processAnnotations(ContactData.class);
+      List<ContactData> contacts = (List<ContactData>) xstream.fromXML(xml);
+      return contacts.stream().map((g) -> new Object[]{g})
+              .collect(Collectors.toList()).iterator();
     }
-    XStream xstream= new XStream();
-    xstream.processAnnotations(ContactData.class);
-    List<ContactData> contacts= (List<ContactData>) xstream.fromXML(xml);
-    return contacts.stream().map((g)-> new Object[]{g})
-            .collect(Collectors.toList()).iterator();
   }
 
   @Test (dataProvider="validContacts")
